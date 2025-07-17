76877974d80f0037c0e85a1e1a61cfd741dc0dde
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-17 17:22:32 +0300

    Script

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Welcome to Password Generator"
+read -p "Enter password length : " len
+
+symbols=("!" "^" "#" "+" "[" "]" "*" "\\" "?" "." "," ";" "@" "&" "{" "}")
+numbers=({0..9})
+lower=({a..z})
+upper=({A..Z})
+
+random_choicer(){
+local -n arr="$1"
+local random_index=$(( RANDOM % ${#arr[@]} ))
+local chosen_element="${arr[$random_index]}"
+echo "$chosen_element"
+}
+password=""
+for (( i=0; i<$len; i++ )); do
+  random_type=$((1 + $RANDOM % 4))
+if [ $random_type == 1 ]; then
+    password+="$(random_choicer symbols)"
+elif [ $random_type == 2 ]; then
+    password+="$(random_choicer numbers)"
+elif [ $random_type == 3 ]; then
+    password+="$(random_choicer lower)"
+elif [ $random_type == 4 ]; then
+    password+="$(random_choicer upper)"
+else 
+    echo "Error!"
+fi
+done
+echo "Generated password : $password"
