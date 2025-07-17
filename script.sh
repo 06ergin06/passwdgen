90ef6b4807565cf3d6042a7b44884afa1a068517
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-17 18:32:36 +0300

    refactor: streamline password generation logic and improve readability

--- script.sh
+++ script.sh
@@ -5,32 +5,26 @@ read -p "Enter password length : " len
 if (($len < 0)); then 
     echo "Password length cannot be negative."
 else 
-symbols=("!" "^" "#" "+" "[" "]" "*" "\\" "?" "." "," ";" "@" "&" "{" "}")
-numbers=({0..9})
-lower=({a..z})
-upper=({A..Z})
+    symbols=("!" "^" "#" "+" "[" "]" "*" "\\" "?" "." "," ";" "@" "&" "{" "}")
+    numbers=({0..9})
+    lower=({a..z})
+    upper=({A..Z})
 
-random_choicer(){
-local -n arr="$1"
-local random_index=$(( RANDOM % ${#arr[@]} ))
-local chosen_element="${arr[$random_index]}"
-echo "$chosen_element"
-}
-password=""
-for (( i=0; i<$len; i++ )); do
-  random_type=$((1 + $RANDOM % 4))
-if [ $random_type == 1 ]; then
-    password+="$(random_choicer symbols)"
-elif [ $random_type == 2 ]; then
-    password+="$(random_choicer numbers)"
-elif [ $random_type == 3 ]; then
-    password+="$(random_choicer lower)"
-elif [ $random_type == 4 ]; then
-    password+="$(random_choicer upper)"
-else 
-    echo "Error!"
-fi
-done
-echo "Generated password : $password"
-
+    random_choicer(){
+    local -n arr="$1"
+    local random_index=$(( RANDOM % ${#arr[@]} ))
+    local chosen_element="${arr[$random_index]}"
+    echo "$chosen_element"
+    }
+    password=""
+    for (( i=0; i<$len; i++ )); do
+        random_type=$((1 + $RANDOM % 4))
+        case $random_type in 
+            1) password+="$(random_choicer symbols)";;
+            2) password+="$(random_choicer numbers)";;
+            3) password+="$(random_choicer lower)";;
+            4) password+="$(random_choicer upper)";;
+        esac
+    done
+    echo "Generated password : $password"
 fi
